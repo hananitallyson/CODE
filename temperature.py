0c241b31c81b361f52af7510aed809c9618cb3cd
tallysonhanani <tallysonahani@gmail.com>
2026-03-10 16:18:06 -0300

    update temperature code

--- temperature.py
+++ temperature.py
@@ -1,5 +1,5 @@
 num_celsius = float(input("Escreva a temperatura em Celsius: "))
 
-num_fahrenheit = (9/5)*num_celsius + 32
+num_fahrenheit = 1.8 * num_celsius + 32
 
 print(num_celsius, "°C ->", num_fahrenheit, "°F")
