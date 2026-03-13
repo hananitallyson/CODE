bac961f47010734cdf0cb22efb53096f838dde1d
tallysonhanani <tallysonahani@gmail.com>
2026-03-13 18:02:44 -0300

    update base_converter any number from any base feature

--- base_converter.py
+++ base_converter.py
@@ -1,37 +1,38 @@
-def to_base(n, base):
-    if base < 2 or base > 36:
+def convert_base(n, from_base, to_base):
+    if to_base < 2 or to_base > 36:
         print("Base must be between 2 and 36.")
         return
-    if n == 0:
+    if n == "0":
         return "0"
+    decimal_number = int(n, from_base)
     symbols = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
     digits = ""
-    while n:
-        remainder = n % base
+    while decimal_number:
+        remainder = decimal_number % to_base
         digits += symbols[remainder]
-        n //= base
+        decimal_number //= to_base
     return digits[::-1]
 
-print("\nWelcome to Base Converter!\n")
+print("\nWelcome to Base Converter!")
+print("Type 'exit' to exit the converter.\n")
 main = True
 
 while main == True:
-    num_list = input("Enter the number and base (ex: 45 2): ")
+    num_list = input("Enter number, current base, and target base (e.g., 45 16 2).\nIf already decimal, just enter number and target base: ")
 
     if num_list == "exit":
         print("")
         main = False
     else:
         num_list = num_list.split(" ")
-        target_num = int(num_list[0])
-        target_base = int(num_list[1])
-
-        match target_base:
-            case 2:
-                print(target_num, "in binary is:", format(target_num, "0b"), "\n")
-            case 8:
-                print(target_num, "in octal is:", format(target_num, "0o"), "\n")
-            case 16:
-                print(target_num, "in hexadecimal is:", format(target_num, "0X"), "\n")
-            case _:
-                print(target_num, "in base", target_base, "is:", to_base(target_num, target_base), "\n")
+        
+        if len(num_list) == 2:
+            target_num = num_list[0]
+            inicial_base = 10
+            target_base = int(num_list[1])
+        else:
+            target_num = num_list[0]
+            inicial_base = int(num_list[1])
+            target_base = int(num_list[2])
+        
+        print(f"{target_num} (base {inicial_base}) -> {convert_base(target_num, inicial_base, target_base)} (base {target_base})\n")
