96290763a02cea57c003111788a6e138a7db428d
tallysonhanani <tallysonahani@gmail.com>
2026-03-13 16:51:08 -0300

    update base_converter exit feature

--- base_converter.py
+++ base_converter.py
@@ -1,6 +1,7 @@
 def to_base(n, base):
     if base < 2 or base > 36:
         print("Base must be between 2 and 36.")
+        return
     if n == 0:
         return "0"
     symbols = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
@@ -11,20 +12,26 @@ def to_base(n, base):
         n //= base
     return digits[::-1]
 
-print("\nWelcome to Base Converter!")
+print("\nWelcome to Base Converter!\n")
+main = True
 
-num_list = input("Enter the number and base (ex: 45 2): ")
-num_list = num_list.split(" ")
+while main == True:
+    num_list = input("Enter the number and base (ex: 45 2): ")
 
-target_num = int(num_list[0])
-target_base = int(num_list[1])
+    if num_list == "exit":
+        print("")
+        main = False
+    else:
+        num_list = num_list.split(" ")
+        target_num = int(num_list[0])
+        target_base = int(num_list[1])
 
-match target_base:
-    case 2:
-        print(target_num, "in binary is:", format(target_num, "0b"), "\n")
-    case 8:
-        print(target_num, "in octal is:", format(target_num, "0o"), "\n")
-    case 16:
-        print(target_num, "in hexadecimal is:", format(target_num, "0X"), "\n")
-    case _:
-        print(target_num, "in base", target_base, "is:", to_base(target_num, target_base), "\n")
+        match target_base:
+            case 2:
+                print(target_num, "in binary is:", format(target_num, "0b"), "\n")
+            case 8:
+                print(target_num, "in octal is:", format(target_num, "0o"), "\n")
+            case 16:
+                print(target_num, "in hexadecimal is:", format(target_num, "0X"), "\n")
+            case _:
+                print(target_num, "in base", target_base, "is:", to_base(target_num, target_base), "\n")
