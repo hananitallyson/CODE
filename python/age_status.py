696c4f81e758042adff492f8aa9321a13c42b787
tallysonhanani <tallysonhanani@gmail.com>
2026-04-01 13:22:44 -0300

    add age_status code

--- python/age_status.py
+++ python/age_status.py
@@ -6,17 +6,13 @@ day_of_birth = int(date_of_birth[0])
 month_of_birth = int(date_of_birth[1])
 year_of_birth = int(date_of_birth[2])
 
-current_date = (datetime.datetime.now().strftime("%d/%m/%Y")).split("/")
+current_date = datetime.date.today()
 
-current_day = int(current_date[0])
-current_month = int(current_date[1])
-current_year = int(current_date[2])
+this_year_age = current_date.year - year_of_birth
 
-this_year_age = current_year - year_of_birth
-
-if current_month == month_of_birth and current_day < day_of_birth or current_month < month_of_birth:
+if current_date.month == month_of_birth and current_date.day < day_of_birth or current_date.month < month_of_birth:
     print(f"Since it hasn't been your birthday yet, your age is {this_year_age - 1}.\n")
-elif current_day == day_of_birth and current_month == month_of_birth:
-    print(f"Happy Birthday! Today is {current_day}/{current_month:02}/{current_year}, your birthday, and your age is {this_year_age}.\n")
+elif current_date.day == day_of_birth and current_date.month == month_of_birth:
+    print(f"Happy Birthday! Today is {current_date.day}/{current_date.month:02}, your birthday, and your age is {this_year_age}.\n")
 else:
     print(f"Since your birthday has already passed, your age is {this_year_age}.\n")
