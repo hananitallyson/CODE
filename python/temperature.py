5e5cbb3873814e63a79abcc583a282cdfbc93084
tallysonhanani <tallysonhanani@gmail.com>
2026-04-09 16:29:33 -0300

    add temperature code

--- python/temperature.py
+++ python/temperature.py
@@ -1,5 +1,18 @@
-num_celsius = float(input("Escreva a temperatura em Celsius: "))
+loop = True
 
-num_fahrenheit = 1.8 * num_celsius + 32
+while loop:
+    print("\ntype 'exit' to leave\nf: Fahrenheit\nk: Kelvin")
+    choice = input("Enter your choice: ")
 
-print(num_celsius, "°C ->", num_fahrenheit, "°F")
+    if choice == "f":
+        num_celsius = float(input("\nEnter the number in Celsius (e.g. 25): "))
+        num_fahrenheit = 1.8 * num_celsius + 32
+        print(num_celsius, "°C ->", num_fahrenheit, "°F")
+    elif choice == "k":
+        num_celsius = float(input("\nEnter the number in Celsius (e.g. 25): "))
+        num_kelvin = num_celsius + 273.15
+        print(num_celsius, "°C ->", num_kelvin, "°K")
+    elif choice == "exit":
+        print("")
+        loop = False
+
