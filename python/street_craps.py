c4522142043818a2215989bf9de2622a83677283
hananitallyson <tallysonhanani@gmail.com>
2026-04-23 23:58:32 -0300

    change street_craps.py

--- python/street_craps.py
+++ python/street_craps.py
@@ -5,12 +5,12 @@ player_points = 0
 bot_points = 0
 
 while game_loop:
-    die_one = random.randint(1,6)
-    die_two = random.randint(1,6)
+    die_one = random.randint(1, 6)
+    die_two = random.randint(1, 6)
     sum_of_dices = die_one + die_two
 
     print("\n-------- STREET CRAPS --------\n")
-    print(f"[{die_one}] [{die_two}]")
+    print(f"[{die_one}] [{die_two}] = {sum_of_dices}")
 
     if sum_of_dices == 7 or sum_of_dices == 11:
         print("The player won!\n")
